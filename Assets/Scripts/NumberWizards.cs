0265354b45c5901a35b09836b8dbb56c93eda2d6
David Pickett <pickettd@gmail.com>
2018-04-25 10:10:37 -1000

    Add 8th

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 public class NumberWizards : MonoBehaviour {
 
 	public Text guessText;
+	private Color32 yellowColor;
+	private Color32 whiteColor;
+	private Color32 blackColor;
 	
 	int max = 1000;
 	int min = 1;
 	int guess;
 	
 	void Start () {
+		yellowColor = new Color32(251,255,0,255);
+		whiteColor = new Color32(255,255,255,255);
+		blackColor = new Color32(0,0,0,0);
+
 		StartGame();
 	}
 	
@@ -36,6 +43,18 @@ public class NumberWizards : MonoBehaviour {
 	
 	void NextGuess () {
 		//guess = (max + min) / 2;
+		if (max > 950) {
+			guessText.color = yellowColor;
+		}
+		else if ((max <= 950)&&(max > 100)) {
+			guessText.color = whiteColor;
+		}
+		else if ((max <= 100)&&(max > 50)) {
+			guessText.color = blackColor;
+		}
+		else if (max <= 50) {
+			guessText.color = yellowColor;
+		}
 		if (max < 5) {
 			guess = -1;
 		}
